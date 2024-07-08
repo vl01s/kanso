aa884700bf21bdca58797df0ed447ebf963c4ba1
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-07-07 20:25:52 -0600

    fix(build): Restrict generated code to `src` dir.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 # Uncomment for debug
 #set -x
 
+! [[ -d ./src ]] && exit 127
+
 C_FILENAMES=$(find ./src -type f -name "*.c")
 COMPILER_FLAGS=("-std=gnu17" "-g" "-Og" "-shared" "-fPIC")
 # -Wall -Werror -pedantic
@@ -12,9 +14,9 @@ LINKER_FLAGS=("-lvulkan" "-lwayland-client" "-lm" "-L$VULKAN_SDK/lib" "-shared")
 OUT_DIR="bin"
 
 wayland-scanner client-header /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
-    ./xdg-shell-client-protocol.h
+    ./src/xdg-shell-client-protocol.h
 wayland-scanner private-code /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
-    ./xdg-shell-protocol.c
+    ./src/xdg-shell-protocol.c
 mkdir -p ./"$OUT_DIR"
 
 gcc $C_FILENAMES -o ./"$OUT_DIR"/libkansoengine.so "${COMPILER_FLAGS[@]}" "${INCLUDE_FLAGS[@]}" "${LINKER_FLAGS[@]}"
