c8e9486e4766c442d03fb7491e1e1d3d98d75c00
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-07-19 22:34:17 -0600

    feat(log): Add `die` and documented code.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- src/log.c
+++ src/log.c
@@ -1,9 +1,36 @@
-#include <log.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <log.h>
 
+/**
+ * Outputs string to STDERR
+ *
+ * @param msg Message string to print
+ */
 void err_msg(char *msg)
 {
   if (msg != NULL) {
     fprintf(stderr, "%s\n", msg);
   }
+}
+
+/**
+ * Kills the program execution
+ *
+ * If status is non-zero and message is given then output will be STDERR
+ *
+ * @param status Exit code for the program
+ * @param msg Message string to print, can be `NULL` if no message is needed
+ */
+void die(const int status, char *msg)
+{
+  // Non-zero status and non-empty message
+  if (status && msg != NULL) {
+    err_msg(msg);
+  // Status is zero and non-empty message
+  } else if (msg != NULL) {
+    printf("%s\n", msg);
+  }
+
+  exit(status);
 }
--- src/log.h
+++ src/log.h
@@ -2,5 +2,6 @@
 #define KANSO_LOG_H
 
 void err_msg(char*);
+void die(const int, char*);
 
 #endif // !KANSO_LOG_H
